5c884ef265189b1b0e39ab2014bcf9e0f1088a4e
sathmika <kulunu.20210786@iit.ac.lk>
2022-06-28 01:32:49 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+
+    },
+    colors: {
+      'purple':'hsl(263, 55%, 52%)',
+      'dark-gray':'hsl(217, 19%, 35%)',
+      'dark-black':'hsl(219, 29%, 14%)',
+      'white':'hsl(0, 0%, 100%)',
+      'light-gray':'hsl(0, 0%, 81%)',
+      'light-gray-blue':'hsl(210, 46%, 95%)',
+    },
+    fontFamily: {
+      sans: ['Barlow Semi Condensed', 'sans-serif'],
+    },
+    fontWeight:{
+      'ex-light':500,
+      'strong':600,
+    },
+  },
+  plugins: [],
+}
